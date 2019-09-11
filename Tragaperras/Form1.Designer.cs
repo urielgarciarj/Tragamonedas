43d89e9ade28a013b76fc4518643a41ffae17637
urielgr1 <urielgarciarj@gmail.com>
2019-09-10 22:14:40 -0500

    Tragamonedas 1.0

---
+++ Tragaperras/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Tragaperras
+{
+    partial class btnMoneyinReturned
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btn20coins = new System.Windows.Forms.Button();
+            this.btn10coins = new System.Windows.Forms.Button();
+            this.btn5coins = new System.Windows.Forms.Button();
+            this.btn2coins = new System.Windows.Forms.Button();
+            this.btn1coins = new System.Windows.Forms.Button();
+            this.lblCoins = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.lblcredits = new System.Windows.Forms.Label();
+            this.btnMoneyReturned = new System.Windows.Forms.Button();
+            this.chbox10 = new System.Windows.Forms.CheckBox();
+            this.chbox5 = new System.Windows.Forms.CheckBox();
+            this.chbox2 = new System.Windows.Forms.CheckBox();
+            this.chbox1 = new System.Windows.Forms.CheckBox();
+            this.chbox20 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(126, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 110);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(280, 58);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(110, 110);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Location = new System.Drawing.Point(434, 58);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(110, 110);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btn20coins
+            // 
+            this.btn20coins.Location = new System.Drawing.Point(719, 67);
+            this.btn20coins.Name = "btn20coins";
+            this.btn20coins.Size = new System.Drawing.Size(53, 26);
+            this.btn20coins.TabIndex = 3;
+            this.btn20coins.Text = "20$";
+            this.btn20coins.UseVisualStyleBackColor = true;
+            this.btn20coins.Click += new System.EventHandler(this.Btn20coins_Click);
+            // 
+            // btn10coins
+            // 
+            this.btn10coins.Location = new System.Drawing.Point(719, 99);
+            this.btn10coins.Name = "btn10coins";
+            this.btn10coins.Size = new System.Drawing.Size(53, 26);
+            this.btn10coins.TabIndex = 4;
+            this.btn10coins.Text = "10$";
+            this.btn10coins.UseVisualStyleBackColor = true;
+            this.btn10coins.Click += new System.EventHandler(this.Btn10coins_Click);
+            // 
+            // btn5coins
+            // 
+            this.btn5coins.Location = new System.Drawing.Point(719, 131);
+            this.btn5coins.Name = "btn5coins";
+            this.btn5coins.Size = new System.Drawing.Size(53, 26);
+            this.btn5coins.TabIndex = 5;
+            this.btn5coins.Text = "5$";
+            this.btn5coins.UseVisualStyleBackColor = true;
+            this.btn5coins.Click += new System.EventHandler(this.Btn5coins_Click);
+            // 
+            // btn2coins
+            // 
+            this.btn2coins.Location = new System.Drawing.Point(719, 163);
+            this.btn2coins.Name = "btn2coins";
+            this.btn2coins.Size = new System.Drawing.Size(53, 26);
+            this.btn2coins.TabIndex = 6;
+            this.btn2coins.Text = "2$";
+            this.btn2coins.UseVisualStyleBackColor = true;
+            this.btn2coins.Click += new System.EventHandler(this.Btn2coins_Click);
+            // 
+            // btn1coins
+            // 
+            this.btn1coins.Location = new System.Drawing.Point(719, 195);
+            this.btn1coins.Name = "btn1coins";
+            this.btn1coins.Size = new System.Drawing.Size(53, 26);
+            this.btn1coins.TabIndex = 7;
+            this.btn1coins.Text = "1$";
+            this.btn1coins.UseVisualStyleBackColor = true;
+            this.btn1coins.Click += new System.EventHandler(this.Btn1coins_Click);
+            // 
+            // lblCoins
+            // 
+            this.lblCoins.AutoSize = true;
+            this.lblCoins.Location = new System.Drawing.Point(726, 42);
+            this.lblCoins.Name = "lblCoins";
+            this.lblCoins.Size = new System.Drawing.Size(45, 13);
+            this.lblCoins.TabIndex = 8;
+            this.lblCoins.Text = "Creditos";
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(247, 229);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(171, 56);
+            this.btnPlay.TabIndex = 9;
+            this.btnPlay.Text = "JUGAR";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.BtnPlay_Click);
+            // 
+            // lblcredits
+            // 
+            this.lblcredits.AutoSize = true;
+            this.lblcredits.Location = new System.Drawing.Point(716, 17);
+            this.lblcredits.Name = "lblcredits";
+            this.lblcredits.Size = new System.Drawing.Size(62, 13);
+            this.lblcredits.TabIndex = 10;
+            this.lblcredits.Text = "CREDITOS";
+            // 
+            // btnMoneyReturned
+            // 
+            this.btnMoneyReturned.Location = new System.Drawing.Point(688, 403);
+            this.btnMoneyReturned.Name = "btnMoneyReturned";
+            this.btnMoneyReturned.Size = new System.Drawing.Size(100, 23);
+            this.btnMoneyReturned.TabIndex = 11;
+            this.btnMoneyReturned.Text = "Sacar Creditos";
+            this.btnMoneyReturned.UseVisualStyleBackColor = true;
+            this.btnMoneyReturned.Click += new System.EventHandler(this.BtnMoneyReturned_Click);
+            // 
+            // chbox10
+            // 
+            this.chbox10.AutoSize = true;
+            this.chbox10.Checked = true;
+            this.chbox10.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chbox10.Location = new System.Drawing.Point(688, 290);
+            this.chbox10.Name = "chbox10";
+            this.chbox10.Size = new System.Drawing.Size(91, 17);
+            this.chbox10.TabIndex = 13;
+            this.chbox10.Text = "Cambio en 10";
+            this.chbox10.UseVisualStyleBackColor = true;
+            // 
+            // chbox5
+            // 
+            this.chbox5.AutoSize = true;
+            this.chbox5.Checked = true;
+            this.chbox5.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chbox5.Location = new System.Drawing.Point(688, 313);
+            this.chbox5.Name = "chbox5";
+            this.chbox5.Size = new System.Drawing.Size(85, 17);
+            this.chbox5.TabIndex = 14;
+            this.chbox5.Text = "Cambio en 5";
+            this.chbox5.UseVisualStyleBackColor = true;
+            // 
+            // chbox2
+            // 
+            this.chbox2.AutoSize = true;
+            this.chbox2.Checked = true;
+            this.chbox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chbox2.Location = new System.Drawing.Point(688, 336);
+            this.chbox2.Name = "chbox2";
+            this.chbox2.Size = new System.Drawing.Size(85, 17);
+            this.chbox2.TabIndex = 15;
+            this.chbox2.Text = "Cambio en 2";
+            this.chbox2.UseVisualStyleBackColor = true;
+            // 
+            // chbox1
+            // 
+            this.chbox1.AutoSize = true;
+            this.chbox1.Checked = true;
+            this.chbox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chbox1.Location = new System.Drawing.Point(688, 359);
+            this.chbox1.Name = "chbox1";
+            this.chbox1.Size = new System.Drawing.Size(85, 17);
+            this.chbox1.TabIndex = 16;
+            this.chbox1.Text = "Cambio en 1";
+            this.chbox1.UseVisualStyleBackColor = true;
+            // 
+            // chbox20
+            // 
+            this.chbox20.AutoSize = true;
+            this.chbox20.Checked = global::Tragaperras.Properties.Settings.Default.true1;
+            this.chbox20.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chbox20.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::Tragaperras.Properties.Settings.Default, "true1", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.chbox20.Location = new System.Drawing.Point(688, 267);
+            this.chbox20.Name = "chbox20";
+            this.chbox20.Size = new System.Drawing.Size(91, 17);
+            this.chbox20.TabIndex = 12;
+            this.chbox20.Text = "Cambio en 20";
+            this.chbox20.UseVisualStyleBackColor = true;
+            // 
+            // btnMoneyinReturned
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Firebrick;
+            this.ClientSize = new System.Drawing.Size(818, 455);
+            this.Controls.Add(this.chbox1);
+            this.Controls.Add(this.chbox2);
+            this.Controls.Add(this.chbox5);
+            this.Controls.Add(this.chbox10);
+            this.Controls.Add(this.chbox20);
+            this.Controls.Add(this.btnMoneyReturned);
+            this.Controls.Add(this.lblcredits);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.lblCoins);
+            this.Controls.Add(this.btn1coins);
+            this.Controls.Add(this.btn2coins);
+            this.Controls.Add(this.btn5coins);
+            this.Controls.Add(this.btn10coins);
+            this.Controls.Add(this.btn20coins);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "btnMoneyinReturned";
+            this.Text = "Tragaperras";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btn20coins;
+        private System.Windows.Forms.Button btn10coins;
+        private System.Windows.Forms.Button btn5coins;
+        private System.Windows.Forms.Button btn2coins;
+        private System.Windows.Forms.Button btn1coins;
+        private System.Windows.Forms.Label lblCoins;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label lblcredits;
+        private System.Windows.Forms.Button btnMoneyReturned;
+        private System.Windows.Forms.CheckBox chbox20;
+        private System.Windows.Forms.CheckBox chbox10;
+        private System.Windows.Forms.CheckBox chbox5;
+        private System.Windows.Forms.CheckBox chbox2;
+        private System.Windows.Forms.CheckBox chbox1;
+    }
+}
+
---
+++ Tragaperras/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tragaperras
+{
+    public partial class btnMoneyinReturned : Form
+    {
+        public const int COINS_20 = 20;
+        public const int COINS_10 = 10;
+        public const int COINS_5 = 5;
+        public const int COINS_2 = 2;
+        public const int COINS_1 = 1;
+        public const int COINS_0 = 0;
+        public const int WINNERMARIO8BITS = 40;
+        public const int WINNERMARIO = 20;
+        public const int WINNERLUIGI = 20;
+        public const int WINNERYOSHI = 30;
+        public const int WINNERBOWSERJR = 10;
+        static PictureBox[] boxes = new PictureBox[3];
+        static Label[] labels = new Label[1];
+        static CheckBox[] checkBox = new CheckBox[5];
+
+        static Image firstimg = Image.FromFile("C:/Users/Uriel/Pictures/mariochido.PNG");
+        static Image secimg = Image.FromFile("C:/Users/Uriel/Pictures/luigichido.PNG");
+        static Image thirdimg = Image.FromFile("C:/Users/Uriel/Pictures/yoshichido.PNG");
+        static Image fourthimg = Image.FromFile("C:/Users/Uriel/Pictures/bowserjrchido.PNG");
+        static Image fifthimg = Image.FromFile("C:/Users/Uriel/Pictures/mario8bitschido.PNG");
+
+        public btnMoneyinReturned()
+        {
+            InitializeComponent();
+            boxes[0] = pictureBox1;
+            boxes[1] = pictureBox2;
+            boxes[2] = pictureBox3;
+            pictureBox1.Image = firstimg;
+            pictureBox2.Image = secimg;
+            pictureBox3.Image = thirdimg;
+            labels[0] = lblCoins;
+            checkBox[0] = this.chbox1;
+            checkBox[1] = this.chbox2;
+            checkBox[2] = this.chbox5;
+            checkBox[3] = this.chbox10;
+            checkBox[4] = this.chbox20;
+        }
+.
+        public static class GlobalData
+        {
+            public static int totalcoins = 0;
+            public static int image = 0;
+            public static int solution = 0;//La solucion en monedas, en int
+            public static string result = "";//La solucion final en monedas, en string
+        }
+
+        private void Btn20coins_Click(object sender, EventArgs e)
+        {
+            GlobalData.totalcoins = GlobalData.totalcoins + COINS_20;
+            this.lblCoins.Text = GlobalData.totalcoins.ToString();
+        }
+
+        private void Btn10coins_Click(object sender, EventArgs e)
+        {
+            GlobalData.totalcoins = GlobalData.totalcoins + COINS_10;
+            this.lblCoins.Text = GlobalData.totalcoins.ToString();
+        }
+
+        private void Btn5coins_Click(object sender, EventArgs e)
+        {
+            GlobalData.totalcoins = GlobalData.totalcoins + COINS_5;
+            this.lblCoins.Text = GlobalData.totalcoins.ToString();
+        }
+
+        private void Btn2coins_Click(object sender, EventArgs e)
+        {
+            GlobalData.totalcoins = GlobalData.totalcoins + COINS_2;
+            this.lblCoins.Text = GlobalData.totalcoins.ToString();
+        }
+
+        private void Btn1coins_Click(object sender, EventArgs e)
+        {
+            GlobalData.totalcoins = GlobalData.totalcoins + COINS_1;
+            this.lblCoins.Text = GlobalData.totalcoins.ToString();
+        }
+
+        private void BtnPlay_Click(object sender, EventArgs e)
+        {
+            if (GlobalData.totalcoins >= 7)
+            {
+                GlobalData.totalcoins = GlobalData.totalcoins - 7;
+                labels[0].Text = GlobalData.totalcoins.ToString();
+                confirm();
+            }
+            else
+            {
+                MessageBox.Show("No hay suficientes créditos");
+            }
+        }
+
+        static async Task SpinSlots1(bool flag)
+        {
+            int i = 0;
+            do
+            {
+                Random rdn = new Random();
+                int a = rdn.Next(0, 6);
+
+                if(a == 1)
+                {
+                    boxes[0].Image = firstimg;
+                }
+                if (a == 2)
+                {
+                    boxes[0].Image = secimg;
+                }
+                if (a == 3)
+                {
+                    boxes[0].Image = thirdimg;
+                }
+                if (a == 4)
+                {
+                    boxes[0].Image = fourthimg;
+                }
+                if (a == 5)
+                {
+                    boxes[0].Image = fifthimg;
+                }
+                i++;
+                await Task.Delay(90);
+            } while (i <= 20);
+        }
+
+        static async Task SpinSlots2(bool flag)
+        {
+            int i = 0;
+            do
+            {
+                Random rdn = new Random();
+                int a = rdn.Next(0, 6);
+
+                if (a == 1)
+                {
+                    boxes[1].Image = firstimg;
+                }
+                if (a == 2)
+                {
+                    boxes[1].Image = secimg;
+                }
+                if (a == 3)
+                {
+                    boxes[1].Image = thirdimg;
+                }
+                if (a == 4)
+                {
+                    boxes[1].Image = fourthimg;
+                }
+                if (a == 5)
+                {
+                    boxes[1].Image = fifthimg;
+                }
+                i++;
+                await Task.Delay(90);
+            } while (i <= 30);
+        }
+
+        static async Task SpinSlots3(bool flag)
+        {
+            int i = 0;
+            do
+            {
+                Random rdn = new Random();
+                int a = rdn.Next(0, 6);
+                if (a == 1)
+                {
+                    boxes[2].Image = firstimg;
+                }
+                if (a == 2)
+                {
+                    boxes[2].Image = secimg;
+                }
+                if (a == 3)
+                {
+                    boxes[2].Image = thirdimg;
+                }
+                if (a == 4)
+                {
+                    boxes[2].Image = fourthimg;
+                }
+                if (a == 5)
+                {
+                    boxes[2].Image = fifthimg;
+                }
+                i++;
+                await Task.Delay(90);
+            } while (i <= 60);
+        }
+
+        static async void confirm()
+        {
+            await elmein();
+            boxes[0].Image = firstimg;
+            boxes[1].Image = firstimg;
+            boxes[2].Image = firstimg;
+            if (boxes[0].Image == boxes[1].Image && boxes[0].Image == boxes[2].Image)
+            {
+                if(boxes[0].Image == fifthimg)
+                {
+                    GlobalData.totalcoins = GlobalData.totalcoins + WINNERMARIO8BITS;
+                    MessageBox.Show("WINNER WINNER CHICKEN DINNER");
+                }
+                else if (boxes[0].Image == firstimg)
+                {
+                    GlobalData.totalcoins = GlobalData.totalcoins + WINNERMARIO;
+                    labels[0].Text = GlobalData.totalcoins.ToString();
+                    MessageBox.Show("PREMIO BUENO");
+                }
+                else if (boxes[0].Image == secimg)
+                {
+                    GlobalData.totalcoins = GlobalData.totalcoins + WINNERLUIGI;
+                    labels[0].Text = GlobalData.totalcoins.ToString();
+                    MessageBox.Show("PREMIO BUENO");
+                }
+                else if (boxes[0].Image == thirdimg)
+                {
+                    GlobalData.totalcoins = GlobalData.totalcoins + WINNERYOSHI;
+                    labels[0].Text = GlobalData.totalcoins.ToString();
+                    MessageBox.Show("PREMIO EXCELENTE");
+                }
+                else if (boxes[0].Image == fourthimg)
+                {
+                    GlobalData.totalcoins = GlobalData.totalcoins + WINNERBOWSERJR;
+                    labels[0].Text = GlobalData.totalcoins.ToString();
+                    MessageBox.Show("PREMIO REGULAR");
+                }
+            }
+            else
+            {
+                MessageBox.Show("¡Suerte para la proxima!");
+                boxes[0].Image = firstimg;
+                boxes[1].Image = secimg;
+                boxes[2].Image = thirdimg;
+            }
+        }
+
+        static async Task elmein()
+        {
+            int i = 0;
+            bool flag = true;
+            SpinSlots1(flag);
+            SpinSlots2(flag);
+            SpinSlots3(flag);
+            do
+            {
+                i++;
+                await Task.Delay(90);
+
+            } while (i < 60);
+        }
+
+        public string solution(int amountcoins, int[] coins)
+        {
+            int i = 0;
+            GlobalData.result = "Solucion: ";
+            while (GlobalData.solution != amountcoins)
+            {
+                i = coins.Length - 1;
+                while (i >= 0)
+                {
+                    if (GlobalData.solution + coins[i] <= amountcoins)//Mientras que las soluciones que se encuentran sean menores a la cantidad a dar cambio
+                    {
+                        GlobalData.solution = GlobalData.solution + coins[i];
+                        GlobalData.result += "\n Una moneda de : " + coins[i];
+                    }
+                    else
+                    {
+                        i = i - 1;
+                    }
+                }
+            }
+            MessageBox.Show(GlobalData.result);
+            GlobalData.totalcoins = 0;
+            labels[0].Text = GlobalData.totalcoins.ToString();
+            GlobalData.result = "";
+            GlobalData.solution = 0;
+            return amountcoins.ToString();
+        }
+
+        private void BtnMoneyReturned_Click(object sender, EventArgs e)
+        {
+            int amountcoins = Int32.Parse(labels[0].Text);
+            List<int> finalcoins = new List<int>();
+            int[] coins = new int[5];
+            coins[0] = COINS_1;
+            coins[1] = COINS_2;
+            coins[2] = COINS_5;
+            coins[3] = COINS_10;
+            coins[4] = COINS_20;
+            for (int i =0; i < checkBox.Length; i++)
+            {
+                if (checkBox[i].Checked)
+                {
+                    finalcoins.Add(coins[i]);
+                }
+            }
+            solution(amountcoins, finalcoins.ToArray());
+        }
+    }
+}
